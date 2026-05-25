b376f346e609a21b18cafc40cdc5d45e921d0875
MimicEnzymes <3021208400@qq.com>
2026-05-26 00:38:37 +0800

    Sync 0.10.0 container tools and render editor

--- src/main/java/com/mimicenzymes/litematicafiller/core/ContainerHighlighter.java
+++ src/main/java/com/mimicenzymes/litematicafiller/core/ContainerHighlighter.java
@@ -1,17 +1,23 @@
 package com.mimicenzymes.litematicafiller.core;
 
+import com.mimicenzymes.litematicafiller.config.Configs;
 import com.mimicenzymes.litematicafiller.render.HighlightRenderer;
 import com.mimicenzymes.litematicafiller.render.HighlightScanner;
 import net.minecraft.client.MinecraftClient;
-import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
 
 public class ContainerHighlighter {
 
     public static void tick(MinecraftClient client) {
+        if (!Configs.ENABLE_MOD.getBooleanValue() || !Configs.HIGHLIGHT_CONTAINERS.getBooleanValue()) {
+            return;
+        }
         HighlightScanner.tick(client);
     }
 
-    public static void onRender(WorldRenderContext context) {
-        HighlightRenderer.getInstance().render(context);
+    public static void onRender(Object context) {
+        if (!Configs.ENABLE_MOD.getBooleanValue() || !Configs.HIGHLIGHT_CONTAINERS.getBooleanValue()) {
+            return;
+        }
+        HighlightRenderer.getInstance().render();
     }
 }
